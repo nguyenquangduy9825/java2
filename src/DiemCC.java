4aa485442d468d3b8f851715f395815931fa5ce5
nguyenquangduy9825 <20233525@eaut.edu.vn>
2026-08-06 09:01:45 +0700

    bài tập ngày 6/8

---
+++ src/DiemCC.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DiemCC {
+    static void main() {
+        System.out.println("mời bạn nhập điểm ");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("mời nhập điểm chuyên cần");
+        double diemCC=sc.nextDouble();
+        System.out.println("mời nhập điểm giữa kỳ");
+        double diemGK=sc.nextDouble();
+        System.out.println("mời nhập điểm cuối kỳ");
+        double diemCK=sc.nextDouble();
+        double  diem=diemCC*0.1+diemGK*0.3+diemCK*0.6;
+        if(diem<5){
+            System.out.println("mày trượt");
+        }
+        else
+            System.out.println("mày đỗ");
+    }
+}
