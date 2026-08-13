07a5aadc9d10c4099a9a56230db1158749eae97a
nguyenquangduy9825 <20233525@eaut.edu.vn>
2026-08-13 09:37:26 +0700

    bài tập 13/6

---
+++ src/Dangnhap.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Dangnhap {
+    private JTextField txtTaiKhoan;
+    private JPasswordField txtMatKhau;
+    private JButton btnDangNhap;
+    private JCheckBox chkGhiNho;
+    private JButton btnThoat;
+}
